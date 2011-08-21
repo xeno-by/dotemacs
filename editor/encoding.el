15fbe6946bcd81278be4c5b16c486c8a8e4d121e
xeno_by <xeno.by@gmail.com>
2011-08-21 16:48:50 +0300

    first stab at addressing differences in line endings between Unix and Windows

--- editor/encoding.el
+++ editor/encoding.el
@@ -1,2 +1,15 @@
-(prefer-coding-system 'utf-8)
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+(set-default default-buffer-file-coding-system 'utf-8-unix)
+
+(defun dos2unix ()
+  "Convert a DOS formatted text buffer to UNIX format"
+  (interactive)
+  (set-buffer-file-coding-system 'undecided-unix nil))
+
+(defun unix2dos ()
+  "Convert a UNIX formatted text buffer to DOS format"
+  (interactive)
+  (set-buffer-file-coding-system 'undecided-dos nil))
 
