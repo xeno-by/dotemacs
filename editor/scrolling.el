c24d86d2a109db3f2859a312e869e69f40747c64
xeno_by <xeno.by@gmail.com>
2011-11-23 17:01:35 +0100

    scrolling

--- editor/scrolling.el
+++ editor/scrolling.el
@@ -2,4 +2,7 @@
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
 
+;; http://emacs-fu.blogspot.com/2009/12/scrolling.html
+(setq scroll-margin 0)
+(setq scroll-conservatively 100000)
 (setq scroll-preserve-screen-position t)
