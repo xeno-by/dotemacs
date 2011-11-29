f6254cc5eb2d0a4fb87790d9e39b12b451f57517
xeno_by <xeno.by@gmail.com>
2011-11-29 10:41:13 +0100

    breakthrough in scrolling performance

---
+++ editor/rendering.el
@@ -0,0 +1,2 @@
+;; this is essential for smooth scrolling on large files with heavyweight modes
+(setq redisplay-dont-pause t)
--- xplatform/windows/speedup.el
+++
@@ -1,2 +0,0 @@
-(setq w32-get-true-file-attributes nil)
-(setq w32-quote-process-args t)
