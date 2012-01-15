48abc149db05c587fc1c744ec6a2fa4f49f5f96c
xeno_by <xeno.by@gmail.com>
2012-01-15 11:30:42 +0100

    evo

--- xplatform/windows/fonts.el
+++ xplatform/windows/fonts.el
@@ -1,6 +1,7 @@
 (setq my-default-font "-outline-Consolas-normal-r-normal-normal-14-90-96-96-c-*-utf-8")
+;;(setq my-default-font "-outline-Consolas-normal-r-normal-normal-36-90-96-96-c-*-utf-8")
 (set-frame-font my-default-font)
 
-(add-hook 'after-make-frame-functions (lambda (frame) 
+(add-hook 'after-make-frame-functions (lambda (frame)
   (modify-frame-parameters frame (list (cons 'font my-default-font)))
   (run-hooks 'after-setting-font-hook 'after-setting-font-hooks)))
